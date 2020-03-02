61ed3044db475a189eff61efae2ee1e3ebb78904
Jesper Brynildsen <jesper_bry@hotmail.com>
2020-03-02 11:58:04 +0100

    Init react project

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="Header">
+      <h2>Prediction of secondary structure</h2>
+    </div>
+  );
+}
+
+export default Header;
