f920692ca8f851a37ac0f4f0bb378103519ae4d0
Jesper Brynildsen <jesper_bry@hotmail.com>
2020-03-03 22:37:39 +0100

    Add list of search result

--- client/src/components/ProteinList.js
+++ client/src/components/ProteinList.js
@@ -1,14 +1,28 @@
 import React from "reactn";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
+import "../styles/ProteinList.css";
+
 class ProteinList extends React.Component {
+  handelOnClick = id => {
+    this.setGlobal({
+      listID: id
+    });
+  };
+
   render() {
     const listArr = this.global.searchData;
     return (
       <div className="idList">
-        <ListGroup>
+        <ListGroup flush>
           {listArr.map((index, id) => (
-            <ListGroupItem key={index}>{listArr[id]}</ListGroupItem>
+            <ListGroupItem
+              key={index}
+              action
+              onClick={() => this.handelOnClick(listArr[id])}
+            >
+              {listArr[id]}
+            </ListGroupItem>
           ))}
         </ListGroup>
       </div>
