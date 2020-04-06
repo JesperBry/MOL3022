eb03c46d871d4ab205616c5827478f5c2a9733d6
olekfur <olekfur@gmail.com>
2020-04-06 17:03:28 +0200

    add skeleton result component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import TitleBar from "./components/TitleBar";
 
 import HomePage from "./components/routes/HomePage";
 import ResultPage from "./components/routes/ResultPage";
+import SearchField from "./components/SearchField";
+import ProteinList from "./components/ProteinList";
+import Result from "./components/Result";
 
 import "./App.css";
 
@@ -17,6 +20,11 @@ class App extends React.Component {
           <Route component={ResultPage} exact path="/result" />
           <Route component={HomePage} exact path="/" />
         </Switch>
+        <SearchField />
+        <Container maxWidth="sm">
+          <ProteinList />
+        </Container>
+        <Result />
       </div>
     );
   }
