e052acfee3d1d2dd7be5bb67c56e5c1c11c5574c
olekfur <olekfur@gmail.com>
2020-04-06 17:03:28 +0200

    add skeleton result component

---
+++ client/src/components/Result.js
@@ -0,0 +1,26 @@
+import React, {Suspense, lazy } from "reactn";
+import { ListGroup, ListGroupItem} from "reactstrap";
+import SkyLight from "react-skylight";
+import SequenceResult from "../components/SequenceResult";
+
+//TODO import "../styles/Result"
+
+class Result extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            visible: true
+        };
+    }
+    
+    render() {
+        return (
+            <div>
+                <SequenceResult pdbID={this.global.listID}/>
+            </div>
+        )
+    }
+}
+
+export default Result;
